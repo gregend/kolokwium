a432c8cbbc650e05f52578ef05dd8e942627fd10
GregEnd <drizzt52@gmail.com>
2017-04-24 11:34:42 +0200

    unit tests for draw_square and draw_circle methods

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+# from app import draw
+
+class TestDrawingMethods(unittest.TestCase):
+    
+    def test_draw_square(self):
+        self.assertEqual(draw_square((1,1), 2), [(1,1), (1,3), (3,1), (3,3)])
+        
+
+    def test_draw_circle(self):
+        self.assertEqual(draw_circle((1,1), 2),[(1,3), (1,-1), (3,1), (-1,1)] )
+
+
+if __name__ == "__main__":
+    unittest.main()
+
