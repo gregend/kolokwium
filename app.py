49d51bb6987fbe9b5df3f3afdb152fbc5943e1d0
GregEnd <drizzt52@gmail.com>
2017-04-24 12:18:54 +0200

    vector_move method implemented

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ def draw_circle(coordinates, radius):
     x, y = coordinates
     circle = [(x, y+radius), (x, y-radius), (x+radius, y), (x-radius, y)]
     return circle
+
+def vector_move(shape_array, vector):
+    moved_shape = []
+    move_x, move_y = vector
+    for coordinates in shape_array:
+        x, y = coordinates
+        new_coordinates = (x+move_x, y+move_y)
+        moved_shape.append(new_coordinates)
+    return moved_shape
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from app import draw_square, draw_circle
+from app import draw_square, draw_circle, vector_move
 
 class TestDrawingMethods(unittest.TestCase):
     
@@ -11,10 +11,10 @@ class TestDrawingMethods(unittest.TestCase):
         self.assertEqual(draw_circle((1,1), 2),[(1,3), (1,-1), (3,1), (-1,1)] )
 
     def test_vector_move(self):
-        shape = [(1,3), (1,-1), (3,1), (-1,1)]
+        shape_array = [(1,3), (1,-1), (3,1), (-1,1)]
         result = [(2,4), (2,0), (4,2), (0,2)]
         vector = (1,1)
-        self.assertEqual(vector_move(shape, vector), result)
+        self.assertEqual(vector_move(shape_array, vector), result)
 
 if __name__ == "__main__":
     unittest.main()
